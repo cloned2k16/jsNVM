0c8ce82e8ccd026650ebdf0986a199140d899676
cloned2k16 <paolo@yaoo.net>
2018-08-28 23:18:58 +0200

    drastically reduce server footprint  by using my own http-d library instead of express  ...

--- server.js
+++ server.js
@@ -1,32 +1,104 @@
-// server.js
+'use strict';
 
-    // ====================================================== Set Up
-    var express             = require('express');
-    var app                 = express();     
-    var router              = express.Router();     
-    var morgan              = require('morgan');                            // log requests to the console (express4)
-    // ====================================================== Configuration
-    var _APP                = {};
-        _APP.PUBLIC_HTML    = '/'; 
-        _APP.LISTEN_PORT    = 8080;
-        _APP.log            = function log() { console.log.apply(this,arguments); }
-        
-    var _                   = _APP;
-    // ====================================================== Application
-    app.use(function(req, res, next) {
-        console.log('adding CORS');
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        next();
-    });    
-    app.use(express.static(__dirname + _.PUBLIC_HTML));                     //
-    app.use(morgan('dev'));                                                 // log every request to the console
-    //app.use(function(req, res){ res.sendStatus(404);});                     // simply NOT FOUND
-    // ====================================================== Main Loop
-	var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || _.LISTEN_PORT
-	,	ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
+//  ----------------------------------- --------------------------- ---------------------------------
+    const   ND              	=   undefined
+    ,       info            	=   {}                                                                      //  to keep in sync, we now fill this from package.json
+    ,       _APP            	=   {}                                                                      //  storing global references here ..
+    ,       Fs             		=   require ('fs')
+    ,       Url             	=   require ('url')
+    ,       Path            	=   require ('path')
+    ,       w4it            	=   require ('w4it')
+    ,       Http            	=   require ('https')
+    ,       HttpD           	=   require ('http-d')
+    ,       LeftPad         	=   require ('left_pad')
+    ,       ChildProc       	=   require ('child_process')
+    ,       Exec            	=   ChildProc.execFileSync 
+    ,       NIM             	=   'nim'
+    ,       _CON            	=   console
+    ,       pathSep         	=   Path.sep
+    ,       strFormat       	=   function ()             {
+                var s   = arguments[0]
+                ,   i   = 1
+                ;
+                return s.replace(/%((%)|s|d|i)/g, function (m) {
+                    var val = arguments[i];
+                    switch (m) {
+                        case '%i':
+                            val = parseInt    (val);
+                            break;
+                        case '%d':
+                            val = parseFloat  (val);
+                            break;
+                    }
+                    i++;
+                    return val;
+                });
+            }
+    ,       qtd             	=   (s)                 =>  { return "'"+s+"'"; }
+    ,       _cout           	=   (t,...a)            =>  { Function.apply.call(t ,_CON,a); }
+    ,       _log            	=   (...a)              =>  { _cout(_CON.log    ,...a); }
+    ,       _wrn            	=   (...a)              =>  { _cout(_CON.warn   ,...a); }
+    ,       _err            	=   (...a)              =>  { _cout(_CON.error  ,...a); }
+    ,       toLocal         	=   (s)                 =>  { return s.replace(new RegExp('/', 'g'),'\\'); }
 	;
 	
-    app.listen(port,ip);
-    _.log("Express server listening on [ "+ip+" : "+_.LISTEN_PORT+" ]");
- 
+			_APP.PUBLIC_HTML    =   '/'; 
+            _APP.BOWER_DIR      =   '/bower_components';
+            _APP.LISTEN_PORT    =   process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 1111 ;
+            _APP.URL_BASE       =   'http://localhost';
+            _APP.out            =   _log
+            _APP.log            =   _log
+            _APP.wrn            =   _wrn
+            _APP.err            =   _err
+            _APP.timeSt         =   (name)  => { return timers[name]= (new Date()).getTime();};
+            _APP.timeEn         =   (name)  => { return (new Date()).valueOf() - timers[name];};
+            _APP.full_list      =   ND
+            
+    let 	_                   =   _APP
+    ,   	me                  =   process
+    ,   	isOS                =   me.arch
+    ,   	args                =   me.argv
+    ,   	cmdLn               =   args[0]
+    ,   	myName              =   args[1]
+    ,   	show                =   true
+    ;
+
+	
+	        Http.head       	=   (url, cb)           =>  {                                               //  keep it simple :D
+                var options=Url.parse(url);
+                options.method='HEAD';
+                var req=Http.request(options,cb);
+                    req.end();
+                return req;    
+            }
+			
+            try{   
+                _.log(HttpD.Name,HttpD.Version);
+                
+                HttpD.setStaticFolders  ([__dirname + _.PUBLIC_HTML,__dirname + _.BOWER_DIR]);
+                HttpD.map               ('/do'     ,function (req,res,query) {
+                    
+                    var fn  = query;
+					_.log("request: ",fn);
+                    return "hola que tal ?";
+                });  
+                
+                HttpD.listen(_.LISTEN_PORT );
+                
+                //TODO!    
+                //_.log('Express server listening on '+_.URL_BASE+':'+_.LISTEN_PORT+'/');
+
+                
+                if (!show){
+                 var child=ChildProc.spawn("cmd",['/C','START',_.URL_BASE+':'+_.LISTEN_PORT, cmd] );
+                    child.on('error',function (err) { _.err(err);    me.exit(-123);  });
+                    child.on('exit', function (code){ _.log(); });
+                }
+            }
+            catch (ex) {
+                 _.err(ex);
+                 me.exit(-123);
+            }       
+			
+
+	
